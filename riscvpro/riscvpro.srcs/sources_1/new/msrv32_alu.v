e96bfd722575cba351816bfe2647f16b12fd0522
Kunwar <kunwarabhay05@gmail.com>
2025-08-15 00:19:48 +0530

    alu fixed

--- riscvpro/riscvpro.srcs/sources_1/new/msrv32_alu.v
+++ riscvpro/riscvpro.srcs/sources_1/new/msrv32_alu.v
@@ -39,6 +39,7 @@ module msrv32_alu(
     wire signed [31:0] signed_op1;
     wire signed [31:0] signed_op2;
     wire [31:0] minus_op2;
+    wire [31:0] adder_op2;
     wire [31:0] sra_result;
     wire [31:0] srl_result;
     wire [31:0] shr_result;
@@ -48,13 +49,14 @@ module msrv32_alu(
     reg [31:0] pre_result;
     
     assign signed_op1=op_1_in;
+    assign signed_op2 = op_2_in;
     assign minus_op2=-op_2_in;
     assign adder_op2= opcode_in[3] == 1'b1 ? minus_op2 : op_2_in;
     assign sra_result= signed_op1>>> op_2_in[4:0];
-    assign srl_result = op_1_in>>>op_2_in[4:0];
+    assign srl_result = op_1_in>>op_2_in[4:0];
     assign shr_result = opcode_in[3]== 1'b1 ? sra_result: srl_result;
     assign sltu_result= op_1_in<op_2_in;
-    assign slt_result= op_1_in[31] ^ op_2_in[31] ? op_1_in[31] : sltu_result;
+    assign slt_result = (op_1_in[31] != op_2_in[31]) ? op_1_in[31] : sltu_result;
     
     always @*
     begin
@@ -67,6 +69,7 @@ module msrv32_alu(
             FUNCT3_SLT : result_out = {31'b0, slt_result};
             FUNCT3_SLTU : result_out = {31'b0, sltu_result};
             FUNCT3_SLL : result_out = op_1_in << op_2_in[4:0];
+            default    : result_out = 32'b0;
         endcase
     end
             
